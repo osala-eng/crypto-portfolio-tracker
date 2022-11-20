65c89e3079ffeac54f2581377b6390ce2e43fed9
Jashon Osala <jashonosala@gmail.com>
2022-11-20 07:11:39 +0300

    Add html tag

--- src/components/Messages.tsx
+++ src/components/Messages.tsx
@@ -2,14 +2,14 @@ import React from 'react'
 
 export const SubmitSuccess = () => {
   return (
-    <div id='submit_success' className='pop-ups load-results'>
-        Success, username accepted</div>
+    <html id='submit_success' className='pop-ups load-results'>
+        Success, username accepted</html>
   );
 };
 
 export const SubmitError = () => {
     return (
-        <div id='submit_error'>Error: Username too short</div>
+        <html id='submit_error'>Error: Username too short</html>
     );
 };
 
